e9579318459a14caaa659288e48706edf20364b3
snkoski <shawnnkoski@gmail.com>
2018-09-22 17:20:36 -0400

    Removing unicode characters is now done on the backend

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -5,11 +5,11 @@ export default class GameCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: false
+      clicked: true
     }
     this.onGameClick = this.onGameClick.bind(this);
     this.renderGameCard = this.renderGameCard.bind(this);
-    this.removeUnicode = this.removeUnicode.bind(this);
+    // this.removeUnicode = this.removeUnicode.bind(this);
   }
 
   onGameClick() {
@@ -18,16 +18,41 @@ export default class GameCard extends Component {
     })
   }
 
-  removeUnicode(string) {
-    string = string.replace(/&#10;/g, " ").replace(/&quot;/g, "\"")
-    return string
-  }
+  // removeUnicode(string) {
+  //   string = string.replace(/&#10;/g, " ")
+  //   .replace(/&quot;|&rdquo;|&ldquo;/g, "\"")
+  //   .replace(/&rsquo;/g, "'")
+  //   .replace(/&#226;&#128;&#147;|&mdash;|&ndash;/g, "-")
+  //   .replace(/&#195;&#137;/g, "É")
+  //   .replace(/&eacute;|&#195;&#169;/g, "é")
+  //   .replace(/&amp;/g, "&")
+  //   .replace(/&trade;/g, "™")
+  //   .replace(/&#9;/g, "")
+  //   .replace(/&gt;/g, ">")
+  //   .replace(/&lsquo;/g, "‘")
+  //   .replace(/&aacute;/g, "á")
+  //   .replace(/&times;/g, "×")
+  //   .replace(/&shy;/g, "")
+  //   .replace(/&uml;/g, "¨")
+  //   .replace(/&reg;/g, "®")
+  //   .replace(/&auml;/g, "ä")
+  //   .replace(/&uuml;/g, "ü")
+  //   .replace(/&plusmn;/g, "±")
+  //   .replace(/&bull;/g, "•")
+  //   .replace(/&ccedil;/g, "ç")
+  //   .replace(/&agrave;/g, "à")
+  //   .replace(/&ecirc;/g, "ê")
+  //   .replace(/&egrave;/g, "è")
+  //   .replace(/&#195;&#156;|&Uuml;/g, "Ü")
+  //
+  //   return string
+  // }
 
   renderGameCard() {
-    const description = this.removeUnicode(this.props.game.description)
+    // const description = this.removeUnicode(this.props.game.description)
     // debugger
     if (this.state.clicked === true) {
-    return (<List.Item onClick={this.onGameClick}>
+    return (<List.Item /*onClick={this.onGameClick}*/>
       <List.Content>
         <List.Header>
           {this.props.game.name}
@@ -35,7 +60,7 @@ export default class GameCard extends Component {
         <img src={this.props.game.thumbnail} />
         <p>Player Count - Min: {this.props.game.min_players} Max: {this.props.game.max_players}</p>
         <p>Play Time - {this.props.game.play_time} minutes</p>
-        <p>Description - {description}</p>
+        <p>Description - {this.props.game.description}</p>
       </List.Content>
     </List.Item>)
     }

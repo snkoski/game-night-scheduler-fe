32dbf83435ce8aaad1cbca51205c62cb21fbda3c
snkoski <shawnnkoski@gmail.com>
2018-09-24 13:23:28 -0400

    Add show more/show less to gme description

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -5,11 +5,12 @@ export default class GameCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: true
+      clicked: false,
+      short_description: true
     }
     this.onGameClick = this.onGameClick.bind(this);
     this.renderGameCard = this.renderGameCard.bind(this);
-    // this.removeUnicode = this.removeUnicode.bind(this);
+    this.render_description = this.render_description.bind(this);
   }
 
   onGameClick() {
@@ -18,49 +19,35 @@ export default class GameCard extends Component {
     })
   }
 
-  // removeUnicode(string) {
-  //   string = string.replace(/&#10;/g, " ")
-  //   .replace(/&quot;|&rdquo;|&ldquo;/g, "\"")
-  //   .replace(/&rsquo;/g, "'")
-  //   .replace(/&#226;&#128;&#147;|&mdash;|&ndash;/g, "-")
-  //   .replace(/&#195;&#137;/g, "É")
-  //   .replace(/&eacute;|&#195;&#169;/g, "é")
-  //   .replace(/&amp;/g, "&")
-  //   .replace(/&trade;/g, "™")
-  //   .replace(/&#9;/g, "")
-  //   .replace(/&gt;/g, ">")
-  //   .replace(/&lsquo;/g, "‘")
-  //   .replace(/&aacute;/g, "á")
-  //   .replace(/&times;/g, "×")
-  //   .replace(/&shy;/g, "")
-  //   .replace(/&uml;/g, "¨")
-  //   .replace(/&reg;/g, "®")
-  //   .replace(/&auml;/g, "ä")
-  //   .replace(/&uuml;/g, "ü")
-  //   .replace(/&plusmn;/g, "±")
-  //   .replace(/&bull;/g, "•")
-  //   .replace(/&ccedil;/g, "ç")
-  //   .replace(/&agrave;/g, "à")
-  //   .replace(/&ecirc;/g, "ê")
-  //   .replace(/&egrave;/g, "è")
-  //   .replace(/&#195;&#156;|&Uuml;/g, "Ü")
-  //
-  //   return string
-  // }
+  render_description() {
+    if (this.state.short_description) {
+      return (<div>
+        {this.props.game.description.substr(0, 200)}...
+        <br/>
+        <a className="game-description" onClick={() => this.setState({short_description: !this.state.short_description})}>Show full description</a>
+      </div>)
+    }
+    return (<div>
+      {this.props.game.description}
+      <br/>
+      <a className="game-description" onClick={() => this.setState({short_description: !this.state.short_description})}>Show less</a>
+    </div>)
+  }
 
   renderGameCard() {
-    // const description = this.removeUnicode(this.props.game.description)
-    // debugger
     if (this.state.clicked === true) {
-    return (<List.Item /*onClick={this.onGameClick}*/>
-      <List.Content>
+    return (<List.Item >
+      <List.Content onClick={this.onGameClick}>
         <List.Header>
           {this.props.game.name}
         </List.Header>
         <img src={this.props.game.thumbnail} />
         <p>Player Count - Min: {this.props.game.min_players} Max: {this.props.game.max_players}</p>
         <p>Play Time - {this.props.game.play_time} minutes</p>
-        <p>Description - {this.props.game.description}</p>
+
+      </List.Content>
+      <List.Content>
+        {this.render_description()}
       </List.Content>
     </List.Item>)
     }

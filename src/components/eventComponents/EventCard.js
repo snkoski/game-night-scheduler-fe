801872ce95608573980faf661bd986d7caefa81f
snkoski <shawnnkoski@gmail.com>
2018-10-12 14:02:43 -0400

    Add moment.js to dependencies. Format event date and time

--- src/components/eventComponents/EventCard.js
+++ src/components/eventComponents/EventCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import * as moment from 'moment';
 
 export default class EventCard extends Component {
   constructor(props) {
@@ -7,17 +8,31 @@ export default class EventCard extends Component {
     this.state = {
 
     }
+    this.formatDate = this.formatDate.bind(this);
+    this.formatTime = this.formatTime.bind(this);
+  }
+
+  formatDate(date) {
+    return moment(date).format("dddd, MMMM Do YYYY");
+  }
+
+  formatTime(time) {
+    return moment(time).format("h:mm a");
   }
 
   render() {
-    console.log("EVENT CARD PROPS:", this.props);
     return (
       <div>
         <p>{this.props.event.name}</p>
-        <p>{this.props.event.date}</p>
-        <p>{this.props.event.time}</p>
-        <p>{this.props.member.username}</p>
+        <p>{this.formatDate(this.props.event.date)}</p>
+        <p>{this.formatTime(this.props.event.time)}</p>
+        <p>Created By: {this.props.member.username}</p>
       </div>
     )
   }
+}
+
+EventCard.propTypes = {
+  event: PropTypes.object.isRequired,
+  member: PropTypes.object.isRequired
 }
